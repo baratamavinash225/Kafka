9f37c48d19e830052b7c1d15db48b59b1eda677f
baratamavinash225 <baratamavinash225@gmail.com>
2019-07-05 15:22:40 +0530

    Delete test.java

--- iotmetricsstream/src/main/java/com/pk/kafka/streams/iot/test.java
+++
@@ -1,8 +0,0 @@
-package com.pk.kafka.streams.iot;
-import java.lang.*;
-public class test {
-    public static void main(String args[]) {
-        String value = "80.5";
-        System.out.println(Float.parseFloat(value));
-    }
-}
